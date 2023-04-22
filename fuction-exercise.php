0e2b41f30cb3cbfd82297edd2d42f08afbad7560
Khashayar <115826321+khashayardadashi@users.noreply.github.com>
2023-04-23 03:10:47 +0400

    Create fuction-exercise.php

---
+++ fuction-exercise.php
@@ -0,0 +1,9 @@
+<?php
+function check1($a,$b,$c){
+    $first_name=$a;
+    $last_name=$b;
+    $age=$c;
+    echo 'Your Name is'.' '.$first_name.' '.$last_name.'</br>'.'And your age is'.' '.$age;
+};
+check1('khashayar','dadashi',18);
+?>
