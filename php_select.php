549cca358630167cc0a4c07d30fec05fd46d4f0f
Khashayar <115826321+khashayardadashi@users.noreply.github.com>
2023-04-23 23:59:16 +0400

    Create php_select.php

---
+++ php_select.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+<select>
+    <?php
+    $i=1390;
+    while($i!=1410){
+        echo '<option>'.$i.'</option>';
+        if ($i==1407){
+            break;
+        }
+        else{
+            $i++;
+        }
+    }
+    ?>
+</select>
+</body>
+</html>
