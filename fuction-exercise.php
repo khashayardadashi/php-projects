0307831a72e077d31fd37660c3dfbeb4b510fc8f
Khashayar <115826321+khashayardadashi@users.noreply.github.com>
2023-05-31 15:27:27 +0400

    Delete fuction-exercise.php

--- fuction-exercise.php
+++
@@ -1,9 +0,0 @@
-<?php
-function check1($a,$b,$c){
-    $first_name=$a;
-    $last_name=$b;
-    $age=$c;
-    echo 'Your Name is'.' '.$first_name.' '.$last_name.'</br>'.'And your age is'.' '.$age;
-};
-check1('khashayar','dadashi',18);
-?>
